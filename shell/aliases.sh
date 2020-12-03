48e0ae0ab75e40534bec9369258f6396318d968e
Hank Wikle <henry.wikle@gmail.com>
2020-12-03 14:13:42 -0700

    Add additional git aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,23 +5,34 @@ alias la="ls -A"
 alias lc="wc -l"
 alias mv="mv -i"
 
+alias p="cd ~/Documents/programming/"
+alias v="vim"
+alias upgrade='sudo apt-get update && sudo apt-get upgrade'
+
 # System Information
 
 alias uname='uname -a'
-
-# Python
-
-alias python="python3"
-alias pip="pip3"
 
 # Git commands
 
 alias gs="git status"
 alias ga="git add"
 alias gc="git commit"
+alias gch="git checkout"
+alias gb="git branch"
+alias gp="git push"
+alias gl="git log"
+alias gm="git merge"
+alias gi="git init"
+alias gr="git remote"
+alias gh="git help"
 
-# Miscellaneous aliases
+# Python
 
-alias v="vim"
-alias upgrade='sudo apt-get update && sudo apt-get upgrade'
-alias p="cd ~/Documents/programming/"
+# alias python="python3"
+# alias pip="pip3"
+
+# Rust
+
+alias cn="cargo new"
+alias cr="cargo run"
